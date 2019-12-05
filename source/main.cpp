b783c093062e18165f7959b6374a4ca14a5bbc9f
Kevin Trocolli <pitok236@gmail.com>
2019-12-05 16:50:47 -0500

    Restructured main function, added function to demo UCP reading/writing

--- source/main.cpp
+++ source/main.cpp
@@ -6,20 +6,19 @@
 #include <stdio.h>
 #include "uct.h"
 
+void demoReadUCPAndWriteSave();
 
 int main(int argc, char **argv) {
-	//TODO: Restructure as a demo menu
 	bool didWrite = false;
 	consoleInit(NULL);
-	
 	userID = getPreUsrAcc();
-	std::vector<CProfile> files;
 	
 	printf("Loading Ultimate Controller Tools...\n");
 	printf("Selected User: 0x%lu %lu\n", (u64)(userID>>64), (u64)userID);	
 	loadProfilesFromConsole(profs);
 	showProfilesFromMemory();
-	printf("Press - to test file load\nPress + to exit");
+	printf("Press - to demo loading UCPs and writing them to save file\nPress + to exit");
+	
 	// Main loop
     while(appletMainLoop())
     {
@@ -29,41 +28,8 @@ int main(int argc, char **argv) {
         //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
 		if ((kDown & KEY_MINUS) && !didWrite) {
-			
-			DIR* dir = opendir("../uct");
-			printf("Dir-listing for '../uct':\n");
-			char dirc[PATH_MAX] = "/uct/";
-	        while ((ent = readdir(dir)))
-	        {
-	        	CProfile tmp;
-	        	std::string s = ent->d_name;
-	        	if (ent->d_name == nullptr) {
-	        		printf("nullptr");
-	        	}
-	        	else if (s.find(".ucp") != std::string::npos) {
-	        		printf("Found possible UCP file: %s\n", ent->d_name);
-	        		memcpy(&dirc[5], ent->d_name, s.length());
-	        		printf(dirc);
-	        		printf("\n");
-	        		loadProfileFromFile(tmp, dirc);
-	        		files.push_back(tmp);
-	        		printf("\n");
-	        	}
-	        	memset(dirc, 0, strlen(dirc));
-	        	strncpy(dirc, "/uct/", sizeof(dirc));
-	        }
-	        closedir(dir);
-	        printf("Overwriting first %d profiles\n", files.size());
-			//profs[0] = files[0];
-			for (int i = 0; i < files.size(); i++) {
-				printf("Overwriting %s with ", profs[i].getNameAsString().c_str());
-				profs[i] = files[i];
-				printf("%s\n", profs[i].getNameAsString().c_str());
-				dumpProfileToConsole(profs[i].raw, i);
-			}
-			printf("Done");
+			demoReadUCPAndWriteSave();
 			didWrite = true;
-			
 		}
         if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
 
@@ -74,3 +40,39 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+void demoReadUCPAndWriteSave() {
+	std::vector<CProfile> files;
+	DIR* dir = opendir("../uct");
+	printf("Dir-listing for '../uct':\n");
+	char dirc[PATH_MAX] = "/uct/";
+	
+    while ((ent = readdir(dir)))
+    {
+        CProfile tmp;
+        std::string s = ent->d_name;
+        if (ent->d_name == nullptr) {
+	        printf("nullptr");
+        }
+        else if (s.find(".ucp") != std::string::npos) {
+	        printf("Found possible UCP file: %s\n", ent->d_name);
+	        memcpy(&dirc[5], ent->d_name, s.length());
+	        printf(dirc);
+	        printf("\n");
+	        loadProfileFromFile(tmp, dirc);
+	        files.push_back(tmp);
+	        printf("\n");
+        }
+        memset(dirc, 0, strlen(dirc));
+        strncpy(dirc, "/uct/", sizeof(dirc));
+    }
+    closedir(dir);
+    printf("Overwriting first %d profiles\n", files.size());
+	//profs[0] = files[0];
+	for (int i = 0; i < files.size(); i++) {
+		printf("Overwriting %s with ", profs[i].getNameAsString().c_str());
+		profs[i] = files[i];
+		printf("%s\n", profs[i].getNameAsString().c_str());
+		dumpProfileToConsole(profs[i].raw, i);
+	}
+	printf("Done");
+}
